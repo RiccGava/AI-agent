35486101c0a9775a99c47c5a9e874b298715fc63
RiccGava <rikk.gava@gmail.com>
2025-08-30 08:09:40 +0200

    issue with args

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
+import sys
 import os
-from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from google.genai import types
+from dotenv import load_dotenv
+import argparse
 
 def main():
-    client = genai.Client(api_key = api_key)
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", "--verbose", action='store_true')
+    parser.add_argument('prompt', nargs='+', help='User prompt')
+    args = parser.parse_args()
+    if not args.prompt:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args.prompt)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if args.verbose:
+        print('User prompt : ' + user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
-    print('Prompt tokens: ' + str(response.usage_metadata.prompt_token_count))
-    print('Response tokens: ' + str(response.usage_metadata.candidates_token_count))
 
 
 if __name__ == "__main__":
     main()
+
