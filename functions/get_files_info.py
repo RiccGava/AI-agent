3c9a931a0df70543edb97a87ec2777fdc35c4e75
RiccGava <rikk.gava@gmail.com>
2025-09-03 06:35:49 +0200

    no output

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,7 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-	full_path = us.path.join(working_directory, directory)
+	full_path = os.path.join(working_directory, directory)
 	abs_working_dir_path = os.path.abspath(working_directory)
 	abs_attempt_path = os.path.abspath(full_path)
 	if abs_attempt_path.startswith(abs_working_dir_path) == False:
@@ -9,11 +9,14 @@ def get_files_info(working_directory, directory="."):
 		return error
 	if os.path.isdir(abs_attempt_path) == False:
 		return f'Error: "{directory}" is not a directory'
-	content = [f for f in os.listdir(abs_path_attempt)]
+	content = [f for f in os.listdir(abs_attempt_path)]
 	for file in content:
-		file_path = os.path.join(abs_attempt_path, name)
-		name = file
-		size = os.path.getsize(file_path)
-		is_dir = os.path.isdir(file_path)
-		print(f'- {name}: file_size={size} bytes, is_dir={is_dir}')
-		
+		try:
+			file_path = os.path.join(abs_attempt_path, name)
+			name = file
+			size = os.path.getsize(file_path)
+			is_dir = os.path.isdir(file_path)
+			print(f'- {name}: file_size={size} bytes, is_dir={is_dir}')
+		except Exception as e:
+			return f'Error: failed to process file. {e}'
+			
