ecd53b00e8e482544098ba2ef0ef8ac979a3214e
RiccGava <rikk.gava@gmail.com>
2025-09-03 06:57:50 +0200

    no output3

--- tests.py
+++ tests.py
@@ -1,9 +1,5 @@
 from functions.get_files_info import get_files_info
 
-
-i = "".join(str(x) for x in get_files_info('calculator', '/'))
-print(i)
-
 print('Result for current directory:\n', get_files_info('calculator', '.'))
 print('Result for pkg directory:\n', get_files_info('calculator', 'pkg'))
 print('Result for /bin directory:\n', get_files_info('calculator', '/bin'))
